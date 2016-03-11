56b99070771434aa2c14db7b18643fcb178b10d0
Vladimír Antoš <vladimirantos@seznam.cz>
2016-03-11 11:21:01 +0100

    Add string utils class

---
+++ Cleaner/Utils/String.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleaner.Utils
+{
+    static class String
+    {
+        public static bool IsUpper(this string text)
+        {
+            return text.All(char.IsUpper);
+        }
+    }
+}
