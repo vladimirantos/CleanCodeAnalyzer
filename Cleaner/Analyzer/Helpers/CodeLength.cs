772aa9a355f05d7a119957e995513f28f8217065
Vladimír Antoš <vladimirantos@seznam.cz>
2016-04-17 15:35:11 +0200

    Change public contructors to private

--- Cleaner/Analyzer/Helpers/CodeLength.cs
+++ Cleaner/Analyzer/Helpers/CodeLength.cs
@@ -12,36 +12,30 @@ namespace Cleaner.Analyzer.Helpers
 {
     public class CodeLength : IAnalyzerHelper<BaseStatistic>
     {
-        private string _code;
+        private readonly string _code;
 
         /// <summary>
         /// Vrací celkový počet řádků.
         /// </summary>
-        public int CountLines => _code.CountLines();
+        private int CountLines => _code.CountLines();
 
         /// <summary>
         /// Vrací počet prázdných řádků.
         /// </summary>
-        public int CountWhitespaceLines => (from line in _code.Lines() where string.IsNullOrWhiteSpace(line) select line).Count();
+        private int CountWhitespaceLines => (from line in _code.Lines() where string.IsNullOrWhiteSpace(line) select line).Count();
 
         /// <summary>
         /// Vrací počet komentářových řádků.
         /// </summary>
-        public int CountCommentLines => (from line in _code.Lines() where Regex.IsMatch(line, @"^\s*//|///|/\*|\*/") select line).Count();
+        private int CountCommentLines => (from line in _code.Lines() where Regex.IsMatch(line, @"^\s*//|///|/\*|\*/") select line).Count();
 
-        public CodeLength(string code)
+        private CodeLength(string code)
         {
             _code = code;
         }
 
-        public BaseStatistic Analyze()
-        {
-            return new CodeLengthStatistics(CountLines, CountCommentLines, CountWhitespaceLines);
-        }
+        public BaseStatistic Analyze() => new CodeLengthStatistics(CountLines, CountCommentLines, CountWhitespaceLines);
 
-        public static BaseStatistic Analyze(string code)
-        {
-            return new CodeLength(code).Analyze();
-        }
+        public static BaseStatistic Analyze(string code) => new CodeLength(code).Analyze();
     }
 }
