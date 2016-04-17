e03052afef0f0975ab5ccfeca33ba518e048b45d
Vladimír Antoš <vladimirantos@seznam.cz>
2016-04-17 12:37:06 +0200

    Add ToString method

--- Cleaner/Entity/CcaVariable.cs
+++ Cleaner/Entity/CcaVariable.cs
@@ -10,11 +10,16 @@ namespace Cleaner.Entity
     {
         public string Name { get; set; }
         public string DataType { get; set; }
-
+        public bool StartWithUnderScore => Name.StartsWith("_");
         public CcaVariable(string name, string dataType)
         {
             Name = name;
             DataType = dataType;
         }
+
+        public override bool Equals(object obj) => Equals(obj as CcaVariable);
+        public override string ToString() => $"{DataType} {Name}";
+
+        private bool Equals(CcaVariable variable) => DataType == variable.DataType;
     }
 }
