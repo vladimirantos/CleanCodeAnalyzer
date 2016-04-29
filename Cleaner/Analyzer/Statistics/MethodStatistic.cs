01da408b0ec07d712b76f239eab713ebbd8e81d9
Vladimír Antoš <vladimirantos@seznam.cz>
2016-04-29 12:38:16 +0200

    Add CcaMethod property

--- Cleaner/Analyzer/Statistics/MethodStatistic.cs
+++ Cleaner/Analyzer/Statistics/MethodStatistic.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cleaner.Entity;
 
 namespace Cleaner.Analyzer.Statistics
 {
     public class MethodStatistic : BaseStatistic
     {
+        public CcaMethod Method { get; set; }
         public int CountArguments { get; set; }
         public int CyclomaticComplexity { get; set; }
     }
