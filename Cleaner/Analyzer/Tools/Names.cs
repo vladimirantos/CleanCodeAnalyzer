62f77a2d0fbfb91869117759224743ad93ce32cb
Vladimír Antoš <vladimirantos@seznam.cz>
2016-04-29 15:37:14 +0200

    Change name metric functionality

--- Cleaner/Analyzer/Tools/Names.cs
+++ Cleaner/Analyzer/Tools/Names.cs
@@ -10,24 +10,31 @@ using Cleaner.Utils.Extensions;
 
 namespace Cleaner.Analyzer.Tools
 {
-    static class Names
+    public static class Names
     {
+        private const string CamelCaseFirstUpper = @"\b[A-Z]+[a-z]+([A-Z]+[a-z]+)*";
+        private const string CamelCaseFirstLower = @"\b[a-z]+[a-z]+([A-Z]+[a-z]+)*";
         private static readonly List<string> _keywords = new List<string>()
         {
             "string", "char", "long", "int", "integer", "float", "double", "array", "list", "dictionary", "stack"
-        }; 
+        };
 
-        public static bool IsCorrect(string name) => !ContainsKeyword(name) && char.IsUpper(name[0]);
+        public static bool IsCorrect(string name) => !ContainsKeyword(name) && IsUpperCamelCase(name);//char.IsUpper(name[0]);
 
-        public static bool IsCorrectVariableName(string name, string dataType) 
-            => !name.Contains(dataType) && name.StartsLower();
+        public static bool IsCorrectVariableName(string name, string dataType)
+            => !name.Contains(dataType) && IsLowerCamelCase(name);
 
         public static bool IsCorrectPropertyName(string name, string dataType)
-            => !name.Contains(dataType) && name.StartsUpper();
+            => !name.Contains(dataType) && IsUpperCamelCase(name);
 
         /// <summary>
         /// Kontroluje, jestli název neobsahuje nějaké klíčové slovo.
         /// </summary>
         private static bool ContainsKeyword(string name) => _keywords.Any(x => name.ToLower().Contains(x));
+
+        private static bool IsUpperCamelCase(string name) => Regex.IsMatch(name, CamelCaseFirstUpper) && !name.Contains("_");
+
+        private static bool IsLowerCamelCase(string name) => Regex.IsMatch(name, CamelCaseFirstLower) && !name.Contains("_");
+        //char.IsUpper(name[0]) && name.ToCharArray().Skip(1).Select(char.IsLower).Count() == name.Length - 1;
     }
 }
