ee70371a97404b15239a95cb6999364633f472a3
Vladimír Antoš <vladimirantos@seznam.cz>
2016-04-29 16:33:23 +0200

    Create another statistics classes

--- Cleaner/Analyzer/Statistics/PropertyStatistic.cs
+++ Cleaner/Analyzer/Statistics/PropertyStatistic.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cleaner.Entity;
 
 namespace Cleaner.Analyzer.Statistics
 {
     public class PropertyStatistic : BaseStatistic
     {
+        public CcaProperty Property { get; set; }
     }
 }
--- Cleaner/Analyzer/Statistics/VariableStatistics.cs
+++ Cleaner/Analyzer/Statistics/VariableStatistics.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cleaner.Entity;
 
 namespace Cleaner.Analyzer.Statistics
 {
     public class VariableStatistics : BaseStatistic
     {
+        public ClassVariable Variable { get; set; }
+
     }
 }
