6e135645667650fc208c8dc201a8dd730bbbdff1
Vladimír Antoš <vladimirantos@seznam.cz>
2016-04-30 23:22:37 +0200

    Public exception

--- Cleaner/CcaException.cs
+++ Cleaner/CcaException.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Cleaner
 {
-    class CcaException : ApplicationException
+    public class CcaException : ApplicationException
     {
         public CcaException()
         {
