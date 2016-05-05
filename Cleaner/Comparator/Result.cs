fabcceaf78d2e7978e64e82735dde693947b9e62
Vladimír Antoš <vladimirantos@seznam.cz>
2016-05-05 09:41:10 +0200

    Initializations of errors

--- Cleaner/Comparator/Result.cs
+++ Cleaner/Comparator/Result.cs
@@ -9,12 +9,15 @@ namespace Cleaner.Comparator
 {
     public class Result
     {
+        private static readonly List<string> MetricNames = new List<string>() { "NAME_LENGTH", "CORRECT_NAMES", "CODE_LINES", "COMMENTS_LINES",
+            "WHITESPACE_LINES", "COUNT_VARIABLES", "COUNT_PROPERTIES", "COUNT_METHODS", "SIMILARITY_METHODS", "COUNT_ARGS", "CYCLOMATIC_COMPLX" };
         public CcaClass Class { get; }
-        public Dictionary<string, int> Errors { get; } = new Dictionary<string, int>();
+        public Dictionary<string, int> Errors { get; private set; }
 
         public Result(CcaClass @class)
         {
             Class = @class;
+            Errors = InitialErrors();
         }
 
         /// <summary>
@@ -26,6 +29,16 @@ namespace Cleaner.Comparator
             if (Errors.TryGetValue(key, out value))
                 Errors[key] = value + 1;
             else Errors.Add(key, 1);
+        }
+
+        private static Dictionary<string, int> InitialErrors()
+        {
+            Dictionary<string, int> errors = new Dictionary<string, int>();
+            foreach (var metricName in MetricNames)
+            {
+                errors.Add(metricName, 0);
+            }
+            return errors;
         }
     }
 }
